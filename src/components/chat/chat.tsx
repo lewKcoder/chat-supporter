39b387e5692adcd141eaa11acea17224505d4822
lewKcoder <7kamimura74@gmail.com>
2023-04-13 01:26:21 +0900

    Fixed style

--- src/components/chat/chat.tsx
+++ src/components/chat/chat.tsx
@@ -1,22 +1,43 @@
 import { Component } from './types';
 import styles from './styles.module.scss';
+import { useEffect, useState } from 'react';
 
 export const Chat: Component = (props) => {
   const { role, content } = props;
+  // const [color, setColor] = useState({
+  //   left: '#fff',
+  //   right: '#000',
+  // });
+
+  // function getRandomColor() {
+  //   const letters = '0123456789ABCDEF';
+  //   let color = '#';
+  //   for (let i = 0; i < 6; i++) {
+  //     color += letters[Math.floor(Math.random() * 16)];
+  //   }
+  //   return color;
+  // }
+
+  // useEffect(() => {
+  //   setColor({ left: getRandomColor(), right: getRandomColor() });
+  // }, []);
 
   return (
-    <div className={`${styles.container}  ${role}`}>
-      <p className={styles.content}>{content}</p>
+    <div
+      className={`${styles.container} ${
+        role === 'user' ? styles.role_user : styles.role_assistant
+      }`}
+    >
+      <p className={styles.content}>
+        <span className={`${styles.icon} ${role}`} />
+        {content}
+      </p>
 
-      <style jsx>{`
+      {/* <style jsx>{`
         .user {
-          background-color: #444654;
+          background: linear-gradient(to right, ${color.left}, ${color.right});
         }
-
-        .assistant {
-          background-color: #343541;
-        }
-      `}</style>
+      `}</style> */}
     </div>
   );
 };
